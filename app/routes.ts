a67abc4b1c004f793815580633edb97fe1c30696
ddieperi <david.dieperink@ik.me>
2026-03-04 12:04:35 +0100

    feat: updated route to match react naming convention

--- app/routes.ts
+++ app/routes.ts
@@ -2,14 +2,14 @@ import { type RouteConfig, index, layout, route } from "@react-router/dev/routes
 
 export default [
   index("routes/home.tsx"),
-  route("projects/:id", "routes/projects/project-detail.tsx"),
-  route("authors", "routes/authors/authors-list.tsx"),
-  route("authors/:id", "routes/authors/author-detail.tsx"),
+  route("projects/:id", "routes/projects/projectDetail.tsx"),
+  route("authors", "routes/authors/authorsList.tsx"),
+  route("authors/:id", "routes/authors/authorDetail.tsx"),
   route("login", "routes/auth/login.tsx"),
   route("register", "routes/auth/register.tsx"),
   layout("routes/middleware.tsx", [
-    route("projects/new", "routes/projects/new-project.tsx"),
-    route("projects/:id/edit", "routes/projects/edit-project.tsx"),
+    route("projects/new", "routes/projects/newProject.tsx"),
+    route("projects/:id/edit", "routes/projects/editProject.tsx"),
   ]),
-  route("*", "routes/not-found.tsx"),
+  route("*", "routes/notFound.tsx"),
 ] satisfies RouteConfig;
