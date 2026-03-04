b82c5577a088f8dc497b2b00b53c2d14580dfb8e
ddieperi <david.dieperink@ik.me>
2026-03-04 11:31:16 +0100

    feat: created routes in the router

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,15 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/home.tsx"),
+  route("projects/:id", "routes/projects/project-detail.tsx"),
+  route("authors", "routes/authors/authors-list.tsx"),
+  route("authors/:id", "routes/authors/author-detail.tsx"),
+  route("login", "routes/auth/login.tsx"),
+  route("register", "routes/auth/register.tsx"),
+  layout("routes/middleware.tsx", [
+    route("projects/new", "routes/projects/new-project.tsx"),
+    route("projects/:id/edit", "routes/projects/edit-project.tsx"),
+  ]),
+  route("*", "routes/not-found.tsx"),
+] satisfies RouteConfig;
